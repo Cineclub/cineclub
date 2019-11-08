5f069ecf1708daad69c9f4c609819fe652d6e187
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2019-11-08 10:44:44 +0000

    Add sidekiq route for logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+
+  constraints Clearance::Constraints::SignedIn.new do
+    mount Sidekiq::Web, at: "/sidekiq", as: :sidekiq
+  end
+
   resources :users, controller: :users, only: :create
   root to: 'home#show'
 end
