505aee695ca6ee86eacca9a80f6c8164988e31b1
Rui Carvalho <ruiccarvalho92@gmail.com>
2020-08-25 23:42:24 +0100

    Add basic unit test for `member_of?`

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,28 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:first_name)}
   end
+
+  describe '#member_of?' do
+    subject do
+      User.create(
+        email: 'example@example.com',
+        password: 'password',
+        first_name: 'User'
+      )
+    end
+    let(:team) { Team.new(name: 'Example Team') }
+
+    context 'when user belongs to team' do
+      it 'returns true' do
+        subject.teams << team
+        expect(subject.member_of?(team)).to be_truthy
+      end
+    end
+
+    context 'when user does not belong to team' do
+      it 'returns false' do
+        expect(subject.member_of?(team)).to be_falsy
+      end
+    end
+  end
 end
