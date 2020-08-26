4534f6071bb38b8550aa571da49ce413eead6af7
Rui Carvalho <ruiccarvalho92@gmail.com>
2020-08-26 22:36:32 +0100

    Add join/leave team capabilities

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,63 @@
+class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update, :destroy, :join, :leave]
+
+  def index
+    @teams = Team.all
+  end
+
+  def show
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      redirect_to @team, notice: 'Team was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @team.update(team_params)
+      redirect_to @team, notice: 'Team was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def join
+    if @team.users << current_user
+      redirect_to @team, notice: 'Team was successfully joined.'
+    else
+      render :show
+    end
+  end
+
+  def leave
+    if @team.users.delete(current_user)
+      redirect_to @team, notice: 'Team was successfully abandoned.'
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(
+      :name
+    )
+  end
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :teams do
+    member do
+      patch 'join'
+      patch 'leave'
+    end
+  end
   resources :users, controller: :users, only: :create
   root to: 'home#show'
 end
