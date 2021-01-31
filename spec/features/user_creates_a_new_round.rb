0bdf6f7f0a3de995feac45e3db1e5ec5910910e9
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-01-31 14:02:50 +0000

    Add feature spec for creating new round

---
+++ spec/features/user_creates_a_new_round.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User triggers the creation of a new round' do
+  given(:user) { create(:user) }
+
+  background do
+    user.teams << create(:team, name: 'Some team')
+    sign_in(user)
+  end
+
+  scenario 'they see a success message' do
+    click_link 'Teams'
+    click_link 'Some team'
+    click_link 'Create New Round'
+
+    expect(page).to have_content 'Round created successfully.'
+    expect(page).to have_content 'No movie has been set yet.'
+  end
+end
