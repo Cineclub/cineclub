f4f5b336494ccaa517bd8678850226c1a63ef520
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-02-05 00:00:32 +0000

    Add policy conditions to spec

--- spec/requests/rounds_api_spec.rb
+++ spec/requests/rounds_api_spec.rb
@@ -7,30 +7,47 @@ describe 'Rounds API', type: :request do
     let(:team) { create(:team) }
     let(:user) { create(:user) }
 
-    context 'when team and user are valid' do
-      it "creates a round for the authenticated user and redirects to the round's show page" do
-        post team_rounds_path(team_id: team.id, as: user)
+    context 'when the user belongs to the team' do
+      before do
+        user.teams << team
+      end
 
-        round = Round.find_by(user: user, team: team)
+      context 'when team and user are valid' do
+        it "creates a round for the authenticated user and redirects to the round's show page" do
+          post team_rounds_path(team_id: team.id, as: user)
 
-        expect(round).to be_present
-        expect(response).to have_http_status(:found)
-        expect(response).to redirect_to round_path(round)
-        expect(flash[:notice]).to eq 'Round created successfully.'
+          round = Round.find_by(user: user, team: team)
+
+          expect(round).to be_present
+          expect(response).to have_http_status(:found)
+          expect(response).to redirect_to round_path(round)
+          expect(flash[:notice]).to eq 'Round created successfully.'
+        end
+      end
+
+      context "when the round can't be created" do
+        before { allow_any_instance_of(Round).to receive(:save).and_return(false) }
+
+        it "redirects to the team's page with an error message" do
+          post team_rounds_path(team_id: team.id, as: user)
+
+          round = Round.find_by(user: user, team: team)
+
+          expect(round).to be_nil
+          expect(response).to redirect_to team_path(id: team.id)
+          expect(flash[:notice]).to eq "Couldn't create round."
+        end
       end
     end
 
-    context "when the round can't be created" do
-      before { allow_any_instance_of(Round).to receive(:save).and_return(false) }
-
-      it "redirects to the team's page with an error message" do
+    context 'when the user does not belong to the team' do
+      it 'redirects the user with an error message' do
         post team_rounds_path(team_id: team.id, as: user)
 
         round = Round.find_by(user: user, team: team)
 
-        expect(round).to be_nil
-        expect(response).to redirect_to team_path(id: team.id)
-        expect(flash[:notice]).to eq "Couldn't create round."
+        expect(round).not_to be_present
+        expect(flash[:alert]).to eq 'You are not authorized to perform this action.'
       end
     end
   end
