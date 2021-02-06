328653db0a9596ac93b5919448733e3386b05194
Rui Carvalho <ruiccarvalho92@gmail.com>
2021-02-06 20:04:37 +0000

    Fix tests

--- spec/factories/directors.rb
+++ spec/factories/directors.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :director do
-    country { Country.find_or_create_by!(code: 'US', name: 'United States') }
     name { 'David Lynch' }
   end
 end
--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Director, type: :model do
   describe 'associations' do
-    it { should belong_to(:country) }
     it { should have_many(:movies) }
   end
 
