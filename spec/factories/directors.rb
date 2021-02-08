26082e184030f246833909008c82eedfed9da7aa
Rui Carvalho <ruiccarvalho92@gmail.com>
2021-02-08 19:08:01 +0000

    Fix movies/directors factories

--- spec/factories/directors.rb
+++ spec/factories/directors.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :director do
     name { 'David Lynch' }
+    sequence(:tmdb_id)
   end
 end
--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :movie do
     original_title
     year { 1990 }
+    sequence(:tmdb_id)
     directors { build_list :director, 1 }
   end
 end
