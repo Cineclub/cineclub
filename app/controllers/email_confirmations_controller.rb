092f8b26c6894f925c76d942bb87a589810fd926
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-02-09 19:41:38 +0000

    Refactor conditional

--- app/controllers/email_confirmations_controller.rb
+++ app/controllers/email_confirmations_controller.rb
@@ -4,12 +4,11 @@ class EmailConfirmationsController < ApplicationController
   def update
     user = User.find_by(email_confirmation_token: params[:token])
 
-    if user.blank?
-      redirect_to confirm_email_path(token: params[:token]), alert: 'You e-mail address could not be confirmed.'
-      return
+    if user.present?
+      user.confirm_email!
+      redirect_to root_path, notice: "Your e-mail address has been confirmed successfully. You're now able to sign in."
+    else
+      redirect_to confirm_email_path(token: params[:token]), alert: 'Your e-mail address could not be confirmed.'
     end
-
-    user.confirm_email!
-    redirect_to root_path, notice: "Your e-mail address has been confirmed successfully. You're now able to sign in."
   end
 end
