a31127ab5caf2e150482f5b2cf945ab938af5779
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-02-10 18:48:18 +0000

    Add TMDb and IMDB logos to round page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  IMDB_MOVIE_BASE_URL = 'https://www.imdb.com/title'.freeze
+  TMDB_MOVIE_BASE_URL = 'https://www.themoviedb.org/movie'.freeze
+
   has_many :directions, dependent: :destroy
   has_many :directors, through: :directions
 
@@ -7,4 +10,14 @@ class Movie < ApplicationRecord
   validates :directors, presence: true
   validates :tmdb_id, presence: true, uniqueness: true
   validates :imdb_id, uniqueness: true, allow_nil: true
+
+  def imdb_url
+    return if imdb_id.nil?
+
+    "#{IMDB_MOVIE_BASE_URL}/#{imdb_id}"
+  end
+
+  def tmdb_url
+    "#{TMDB_MOVIE_BASE_URL}/#{tmdb_id}"
+  end
 end
