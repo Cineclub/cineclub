ca2ddf1351c90cfad220e0ff18a5b00a587fe42e
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-02-11 21:28:54 +0000

    Change movie for string in helper param

--- app/helpers/movie_helper.rb
+++ app/helpers/movie_helper.rb
@@ -2,13 +2,11 @@ module MovieHelper
   IMDB_MOVIE_BASE_URL = 'https://www.imdb.com/title'.freeze
   TMDB_MOVIE_BASE_URL = 'https://www.themoviedb.org/movie'.freeze
 
-  def imdb_url(movie)
-    return if movie.imdb_id.nil?
-
-    "#{IMDB_MOVIE_BASE_URL}/#{movie.imdb_id}"
+  def imdb_url(imdb_id)
+    "#{IMDB_MOVIE_BASE_URL}/#{imdb_id}" if imdb_id.present?
   end
 
-  def tmdb_url(movie)
-    "#{TMDB_MOVIE_BASE_URL}/#{movie.tmdb_id}"
+  def tmdb_url(tmdb_id)
+    "#{TMDB_MOVIE_BASE_URL}/#{tmdb_id}" if tmdb_id.present?
   end
 end
