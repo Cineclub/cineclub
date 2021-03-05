8f4cb3ecde7f626dc6d362a92c663622adf46b70
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-03-05 16:25:35 +0000

    Calculate round position in team's rounds

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,8 @@ class Round < ApplicationRecord
   belongs_to :team
   belongs_to :movie, optional: true
   has_many :screenings, dependent: :destroy
+
+  def number_in_team
+    team.rounds.order(created_at: :asc).where('created_at < ?', created_at).count + 1
+  end
 end
--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Round, type: :model do
     it { should belong_to(:movie).optional }
     it { should have_many(:screenings) }
   end
+
+  describe '#number_in_team' do
+    let(:team) { create(:team) }
+
+    it "returns the round's position in a team's context according to its creation date" do
+      oldest_round = create(:round, team: team, created_at: 2.days.ago)
+      in_between_round = create(:round, team: team, created_at: 1.day.ago)
+      newest_round = create(:round, team: team)
+
+      expect(oldest_round.number_in_team).to eq 1
+      expect(in_between_round.number_in_team).to eq 2
+      expect(newest_round.number_in_team).to eq 3
+    end
+  end
 end
