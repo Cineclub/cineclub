30a62ea226682fff069cb4bd4d672cf178fc0adc
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-03-07 16:33:08 +0000

    Rename to index

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,7 +4,7 @@ class Round < ApplicationRecord
   belongs_to :movie, optional: true
   has_many :screenings, dependent: :destroy
 
-  def number_in_team
-    team.rounds.order(created_at: :asc).where('created_at < ?', created_at).count + 1
+  def index
+    team.rounds.where('id < ?', id).count + 1
   end
 end
--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe Round, type: :model do
     it { should have_many(:screenings) }
   end
 
-  describe '#number_in_team' do
+  describe '#index' do
     let(:team) { create(:team) }
 
     it "returns the round's position in a team's context according to its creation date" do
-      oldest_round = create(:round, team: team, created_at: 2.days.ago)
-      in_between_round = create(:round, team: team, created_at: 1.day.ago)
-      newest_round = create(:round, team: team)
+      first_round = create(:round, team: team)
+      second_round = create(:round, team: team)
+      third_round = create(:round, team: team)
 
-      expect(oldest_round.number_in_team).to eq 1
-      expect(in_between_round.number_in_team).to eq 2
-      expect(newest_round.number_in_team).to eq 3
+      expect(first_round.index).to eq 1
+      expect(second_round.index).to eq 2
+      expect(third_round.index).to eq 3
     end
   end
 end
