3dc0d921ceef4a88ef0b4d0c51267f180af323bc
Rui Carvalho <ruiccarvalho92@gmail.com>
2021-03-17 20:34:24 +0000

    Validate "invitability"

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,4 +4,11 @@ class Invitation < ApplicationRecord
   belongs_to :team
 
   validates :invitee, uniqueness: { scope: %i[team accepted] }, unless: :accepted?
+  validate :invitee_not_in_team, on: :create
+
+  private
+
+  def invitee_not_in_team
+    errors.add(:invitee, 'already belongs to the team') if invitee.member_of?(team)
+  end
 end
