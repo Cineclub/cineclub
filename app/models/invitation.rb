bb4995bf3f55fd1a90a1aebb8c10e2ef4e46253c
Rui Carvalho <ruiccarvalho92@gmail.com>
2021-03-17 20:00:41 +0000

    Fix linter complaints

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,5 +3,5 @@ class Invitation < ApplicationRecord
   belongs_to :inviter, class_name: 'User'
   belongs_to :team
 
-  validates :invitee, uniqueness: { scope: [:inviter, :team, :accepted] }, unless: :accepted?
+  validates :invitee, uniqueness: { scope: %i[inviter team accepted] }, unless: :accepted?
 end
