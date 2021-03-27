e04978577da7eb56f2bbfc8ca7f488a7f918d7d3
Rui Carvalho <ruiccarvalho92@gmail.com>
2021-03-27 19:47:11 +0000

    Improve specs for the Invitation model

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,34 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
-  subject { create(:invitation) }
-
   describe 'associations' do
+    subject { create(:invitation) }
     it { should belong_to(:invitee).class_name('User') }
     it { should belong_to(:inviter).class_name('User') }
     it { should belong_to(:team) }
   end
 
   describe 'validations' do
-    describe 'validate uniqueness of the invitee among pending invites for that team'
-    context 'invitation is pending' do
-      it 'does not permit foo' do
-        new_invitation = Invitation.new(invitee: subject.invitee, inviter: subject.inviter, team: subject.team)
-        expect(new_invitation.valid?).to be_falsey
-        expect(new_invitation.errors[:invitee]).to include('has already been taken')
+    describe 'validate uniqueness of the invitee among pending invites for that team' do
+      let(:inviter) { create(:user) }
+      let(:invitee) { create(:user) }
+      let(:team) { create(:team) }
+      subject { build(:invitation, inviter: inviter, invitee: invitee, team: team) }
+
+      context 'pending invitation of `invitee` to join `team` exists' do
+        before { create(:invitation, inviter: inviter, invitee: invitee, team: team) }
+
+        it 'does not permit creating another invitation for the same user to the same team' do
+          expect(subject).not_to be_valid
+          expect(subject.errors[:invitee]).to include('has already been taken')
+        end
       end
 
-      it 'permits bar' do
-        subject.update(dismissed_at: Time.current)
+      context 'dismissed invitation of `invitee` to join `team` exists' do
+        before { create(:invitation, inviter: inviter, invitee: invitee, team: team, dismissed_at: Time.current) }
 
-        new_invitation = Invitation.new(invitee: subject.invitee, inviter: subject.inviter, team: subject.team)
-        expect(new_invitation.valid?).to be_truthy
+        it 'permits creating another invitation for the same user to the same team' do
+          expect(subject).to be_valid
+        end
       end
 
-      it 'allows inviting the same user to another team' do
-        other_team = build(:team)
-        new_invitation = Invitation.new(invitee: subject.invitee, inviter: subject.inviter, team: other_team)
-        expect(new_invitation.valid?).to be_truthy
+      context 'inviting a user different than `invitee`' do
+        let(:another_invitee) { create(:user) }
+        subject { build(:invitation, inviter: inviter, invitee: another_invitee, team: team) }
+
+        it 'allows inviting the same user to another team' do
+          expect(subject).to be_valid
+        end
       end
     end
   end
