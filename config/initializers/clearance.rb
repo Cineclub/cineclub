e295d981a4d808dfce2be4136bd2e01c2b448d77
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2021-09-24 21:27:53 +0100

    Fix Clearance autoload error

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,8 @@
-Clearance.configure do |config|
-  config.mailer_sender = "no-reply@cineclub.app"
-  config.rotate_csrf_on_sign_in = true
-  config.routes = true
-  config.sign_in_guards = [ConfirmedUserGuard]
+Rails.application.config.to_prepare do
+  Clearance.configure do |config|
+    config.mailer_sender = "no-reply@cineclub.app"
+    config.rotate_csrf_on_sign_in = true
+    config.routes = true
+    config.sign_in_guards = [ConfirmedUserGuard]
+  end
 end
